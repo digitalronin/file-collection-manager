59b6c0c090e1c3e57d7fdedcb544b738d4934916
David Salgado <david@digitalronin.com>
2019-05-26 16:57:25 +0100

    Add initial version of the auditor script
    It doesn't check the last N bytes of each file,
    but other than that it does what the spec says.

---
+++ bin/auditor.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'digest/sha1'
+require 'json'
+
+BYTES_TO_HASH = 1_000_000
+
+IGNORE_FILE_PATTERNS = [
+]
+
+def main(root)
+  files = get_file_data(root)
+  {
+    root: root,
+    timestamp: Time.now,
+    files: files
+  }
+end
+
+def get_file_data(root)
+  get_filenames(root).map {|f| filedata(f)}
+end
+
+def filedata(filepath)
+  bytes = File.read(filepath, BYTES_TO_HASH)
+  {
+    path: filepath,
+    name: File.basename(filepath),
+    size: File.size(filepath),
+    sha1: bytes.nil? ? nil : Digest::SHA1.hexdigest(File.read(filepath, BYTES_TO_HASH))
+  }
+end
+
+def get_filenames(root)
+  Dir.glob("#{root}/**/*")
+    .find_all {|f| FileTest.file?(f)}
+    .reject {|f| IGNORE_FILE_PATTERNS.find {|rexp| rexp.match(f)}}
+end
+
+dir = ARGV.shift
+puts main(dir).to_json
