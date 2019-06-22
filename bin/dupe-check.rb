b81f07c02b37a6816fa6cdb0127ed8046fbcb312
David Salgado <david@digitalronin.com>
2019-06-22 16:13:49 +0100

    Alter dupe-checker to output full file metadata

--- bin/dupe-check.rb
+++ bin/dupe-check.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'pp'
 require './lib/file_dupes'
 
 def main(json)
   files = JSON.parse(json).fetch('files')
   dupes = FileDupes.new(files).get_duplicates()
   dupes.each do |list|
-    list.each {|file| puts file['path']}
-    puts
+    list.each do |file|
+      pp(file)
+      puts
+    end
   end
 end
 
 json = $stdin.read
 main json
-
-
